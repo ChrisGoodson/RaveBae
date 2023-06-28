aa659e129730d292a91688bc10c9a8688bda318a
Chris <f.chrisgoodson@icloud.com>
2023-06-28 17:32:29 -0500

    Initial Commit

---
+++ RaveBae/RaveBaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaveBaeApp.swift
+//  RaveBae
+//
+//  Created by Chris on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct RaveBaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
